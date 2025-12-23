d6c8648b7d5dd539fb0d0d0b4f2d49299d31576c
Takoizu <vldggg@bk.ru>
2025-12-23 12:03:16 +0500

    first commit

--- public/zd/Practica10/script.js
+++ public/zd/Practica10/script.js
@@ -4,10 +4,16 @@ const img = document.querySelector("img");
 
 h1.textContent = "Заголовок";
 p.textContent = "Текст параграфа";
-img.src = "https://picsum.photos/300/200";
+img.src = "https://picsum.photos/999/200";
 
 const div = document.createElement("div");
 div.style.background = "black";
 div.style.color = "white";
 div.textContent = "Новый блок";
+div.setAttribute("style", "width: 100px; height: 100px; border: 2px solid #1d1d1d; background: red");
+
 document.body.append(div);
+
+div.innerHTML = "<p>Это целый тег из JS</p>";
+
+
